01488cb826330f9f87850c7b14310fc1d12c76d2
bansibirajdar <bansi.birajdar7@gmail.com>
2019-10-25 05:16:28 +0530

    change
    Signed-off-by: bansibirajdar <bansi.birajdar7@gmail.com>

--- Assignment2_9.py
+++ Assignment2_9.py
@@ -1,16 +1,8 @@
 '''Write a program which accept number from user and return number of digits in that number.
 Input : 5187934 Output : 7'''
-icnt=None;
+
+
 def CountDigit(no):
-    global icnt;
-    print (icnt);
-    if(no==0):
-        #print("Please enter the number ");
-        return icnt;
-    else:
-        icnt+=1;
-        CountDigit(no//10);
-'''def CountDigit(no):
     if(no==0):
         print("Please enter the number");
         return 0;
@@ -18,7 +10,7 @@ def CountDigit(no):
     while no!=0:
         icnt+=1;
         no=no//10;
-    return icnt; '''      
+    return icnt;      
 
 def main():
     no=int(input("Enter the number=="))
